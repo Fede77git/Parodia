0d2c18fe6e3ea38efd30e7b0229b9ee7d69101e8
Fede77git <fedeecarp11@gmail.com>
2024-06-23 18:01:36 -0300

    score system

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,28 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
     public GameObject effectObj;
-    private int scoreCount = 0;
+    private int scoreCount;
+    public TMP_Text scoreText;
+
     private void Start()
     {
         effectObj.SetActive(false);
+        scoreCount = 0;
+        SetText();
     }
     private void OnTriggerEnter(Collider otherCollider)
     {
         if (otherCollider.gameObject.CompareTag("Ball"))
         {
             effectObj.SetActive (true);
-            Debug.Log("Score!");
+           
 
-            scoreCount++;
-            Debug.Log("Score:" + scoreCount);
+            scoreCount = scoreCount + 1;
+            SetText();
+           
 
             StartCoroutine(DisableEffect());
         }
     }
+
+
+    private void SetText()
+    {
+        scoreText.text = " " + scoreCount.ToString();
+        if (scoreCount >= 5) 
+        {
+            Debug.Log("WIN");
+
+        }
+    }
+
 
     private IEnumerator DisableEffect()
     {
