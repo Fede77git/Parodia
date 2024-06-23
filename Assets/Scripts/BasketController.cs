587f4cf38db37fdbc9eab70dcad76e815e6a2c1b
Fede77git <fedeecarp11@gmail.com>
2024-06-23 16:46:08 -0300

    base game

--- Assets/Scripts/BasketController.cs
+++ Assets/Scripts/BasketController.cs
@@ -4,15 +4,91 @@ using UnityEngine;
 
 public class BasketController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float MoveSpeed = 10;
+    public Transform Ball;
+    public Transform PosDribble;
+    public Transform PosOverHead;
+    public Transform Arms;
+    public Transform Target;
 
-    // Update is called once per frame
+   
+    private bool IsBallInHands = true;
+    private bool IsBallFlying = false;
+    private float T = 0;
+
+   
     void Update()
     {
+
         
+        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        transform.position += direction * MoveSpeed * Time.deltaTime;
+        transform.LookAt(transform.position + direction);
+
+        
+        if (IsBallInHands)
+        {
+
+            
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Ball.position = PosOverHead.position;
+                Arms.localEulerAngles = Vector3.right * 180;
+
+               
+                transform.LookAt(Target.parent.position);
+            }
+
+           
+            else
+            {
+                Ball.position = PosDribble.position + Vector3.up * Mathf.Abs(Mathf.Sin(Time.time * 5));
+                Arms.localEulerAngles = Vector3.right * 0;
+            }
+
+           
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                IsBallInHands = false;
+                IsBallFlying = true;
+                T = 0;
+            }
+        }
+
+       
+        if (IsBallFlying)
+        {
+            T += Time.deltaTime;
+            float duration = 0.66f;
+            float t01 = T / duration;
+
+           
+            Vector3 A = PosOverHead.position;
+            Vector3 B = Target.position;
+            Vector3 pos = Vector3.Lerp(A, B, t01);
+
+            
+            Vector3 arc = Vector3.up * 5 * Mathf.Sin(t01 * 3.14f);
+
+            Ball.position = pos + arc;
+
+            
+            if (t01 >= 1)
+            {
+                IsBallFlying = false;
+                Ball.GetComponent<Rigidbody>().isKinematic = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (!IsBallInHands && !IsBallFlying)
+        {
+
+            IsBallInHands = true;
+            Ball.GetComponent<Rigidbody>().isKinematic = true;
+        }
     }
 }
