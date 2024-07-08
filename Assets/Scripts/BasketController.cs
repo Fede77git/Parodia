d30e74caa5d50db9f7b47601e676e5745c5a22a0
Fede77git <fedeecarp11@gmail.com>
2024-07-08 17:06:20 -0300

    implementacion jugador 2

--- Assets/Scripts/BasketController.cs
+++ Assets/Scripts/BasketController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class BasketController : MonoBehaviour
 {
+
+    public enum PlayerNumber { Player1, Player2 }
+    public PlayerNumber playerNumber;
+
+
     public float MoveSpeed = 7;
     public Transform Ball;
     public Transform PosDribble;
@@ -27,42 +32,49 @@ public class BasketController : MonoBehaviour
     void Update()
     {
 
-        
-        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        Vector3 move = direction * MoveSpeed * Time.deltaTime;
 
-        //transform.position += direction * MoveSpeed * Time.deltaTime;
-        //transform.LookAt(transform.position + direction);
-        characterController.Move(move);
+        Vector3 direction = Vector3.zero;
 
-        if (direction != Vector3.zero)
+        if (playerNumber == PlayerNumber.Player1)
         {
-            transform.forward = direction;
-        }
+            direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-
-        if (IsBallInHands)
-        {
-
-            
-            if (Input.GetKey(KeyCode.Space))
+            if (Input.GetKey(KeyCode.V))
             {
                 Ball.position = PosOverHead.position;
                 Arms.localEulerAngles = Vector3.right * 180;
-
-               
                 transform.LookAt(Target.parent.position);
             }
-
-           
             else
             {
                 Ball.position = PosDribble.position + Vector3.up * Mathf.Abs(Mathf.Sin(Time.time * 5));
                 Arms.localEulerAngles = Vector3.right * 0;
             }
 
-           
-            if (Input.GetKeyUp(KeyCode.Space))
+            if (Input.GetKeyUp(KeyCode.V))
+            {
+                IsBallInHands = false;
+                IsBallFlying = true;
+                T = 0;
+            }
+        }
+        else if (playerNumber == PlayerNumber.Player2)
+        {
+            direction = new Vector3(Input.GetAxisRaw("Horizontal2"), 0, Input.GetAxisRaw("Vertical2"));
+
+            if (Input.GetKey(KeyCode.B))
+            {
+                Ball.position = PosOverHead.position;
+                Arms.localEulerAngles = Vector3.right * 180;
+                transform.LookAt(Target.parent.position);
+            }
+            else
+            {
+                Ball.position = PosDribble.position + Vector3.up * Mathf.Abs(Mathf.Sin(Time.time * 5));
+                Arms.localEulerAngles = Vector3.right * 0;
+            }
+
+            if (Input.GetKeyUp(KeyCode.B))
             {
                 IsBallInHands = false;
                 IsBallFlying = true;
@@ -70,24 +82,27 @@ public class BasketController : MonoBehaviour
             }
         }
 
-       
+        Vector3 move = direction * MoveSpeed * Time.deltaTime;
+        characterController.Move(move);
+
+        if (direction != Vector3.zero)
+        {
+            transform.forward = direction;
+        }
+
         if (IsBallFlying)
         {
             T += Time.deltaTime;
             float duration = 0.66f;
             float t01 = T / duration;
 
-           
             Vector3 A = PosOverHead.position;
             Vector3 B = Target.position;
             Vector3 pos = Vector3.Lerp(A, B, t01);
-
-            
             Vector3 arc = Vector3.up * 5 * Mathf.Sin(t01 * 3.14f);
 
             Ball.position = pos + arc;
 
-            
             if (t01 >= 1)
             {
                 IsBallFlying = false;
@@ -98,10 +113,8 @@ public class BasketController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-
         if (!IsBallInHands && !IsBallFlying)
         {
-
             IsBallInHands = true;
             Ball.GetComponent<Rigidbody>().isKinematic = true;
         }
