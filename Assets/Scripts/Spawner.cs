d7047559ce29bca82c9044e378676da39414ed75
Fede77git <fedeecarp11@gmail.com>
2026-05-08 15:22:48 -0300

    Spawner fix y build

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,18 +8,41 @@ public class Spawner : MonoBehaviour
     public bool stopSpawn = false;
     public float spawnTime;
     public float spawnDelay;
+    
+    private GameObject currentSpawnedObject;
+    private float timer = 0f;
 
     void Start()
     {
-        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+        timer = spawnTime;
+    }
+
+    void Update()
+    {
+        if (stopSpawn) return;
+
+        if (currentSpawnedObject == null || !currentSpawnedObject.activeInHierarchy)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                SpawnObject();
+                timer = spawnDelay;
+            }
+        }
     }
 
     public void SpawnObject()
     {
-        Instantiate(spawner, transform.position, transform.rotation);
-        if (stopSpawn)
+        if (currentSpawnedObject == null)
         {
-            CancelInvoke("SpawnObject");
+            currentSpawnedObject = Instantiate(spawner, transform.position, transform.rotation);
+        }
+        else
+        {
+            currentSpawnedObject.transform.position = transform.position;
+            currentSpawnedObject.transform.rotation = transform.rotation;
+            currentSpawnedObject.SetActive(true);
         }
     }
 }
