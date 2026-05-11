68b453a5e2bc53835f2b4e0c7226bcbf564af3f4
Fede77git <fedeecarp11@gmail.com>
2026-05-11 15:39:59 -0300

    add datospartidaManager para stats de rpg
    rpg - roguelite

---
+++ Assets/Scripts/DatosPartidaManager.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+
+[System.Serializable]
+public class DatosJugador
+{
+    public int estrellas;
+    public int puntosMoneda;
+    public int nivelVelocidad = 1;
+    public int nivelTamano = 1;
+    public int nivelCooldownTiro = 1;
+    public int nivelCooldownDash = 1;
+}
+
+public enum TipoEstadistica
+{
+    Velocidad,
+    Tamano,
+    CooldownTiro,
+    CooldownDash
+}
+
+public class DatosPartidaManager : MonoBehaviour
+{
+    public static DatosPartidaManager Instance { get; private set; }
+
+    public DatosJugador[] jugadores = new DatosJugador[4];
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (jugadores[i] == null)
+            {
+                jugadores[i] = new DatosJugador();
+            }
+        }
+    }
+
+    public void SumarEstrellas(int playerID, int cantidad)
+    {
+        if (playerID >= 0 && playerID < 4)
+        {
+            jugadores[playerID].estrellas += cantidad;
+        }
+    }
+
+    public void SumarMonedas(int playerID, int cantidad)
+    {
+        if (playerID >= 0 && playerID < 4)
+        {
+            jugadores[playerID].puntosMoneda += cantidad;
+        }
+    }
+
+    public void AumentarEstadistica(int playerID, TipoEstadistica estadistica)
+    {
+        if (playerID >= 0 && playerID < 4)
+        {
+            switch (estadistica)
+            {
+                case TipoEstadistica.Velocidad:
+                    jugadores[playerID].nivelVelocidad++;
+                    break;
+                case TipoEstadistica.Tamano:
+                    jugadores[playerID].nivelTamano++;
+                    break;
+                case TipoEstadistica.CooldownTiro:
+                    jugadores[playerID].nivelCooldownTiro++;
+                    break;
+                case TipoEstadistica.CooldownDash:
+                    jugadores[playerID].nivelCooldownDash++;
+                    break;
+            }
+        }
+    }
+}
